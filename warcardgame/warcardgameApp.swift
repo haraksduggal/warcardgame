e07242bfae87a021a6639d4a3b68b9c7b46fc795
Haraks Kaur Duggal <haraksduggal@gmail.com>
2024-07-28 16:57:28 +0530

    Initial Commit

---
+++ warcardgame/warcardgameApp.swift
@@ -0,0 +1,17 @@
+//
+//  warcardgameApp.swift
+//  warcardgame
+//
+//  Created by Haraks Kaur Duggal on 28/07/24.
+//
+
+import SwiftUI
+
+@main
+struct warcardgameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
